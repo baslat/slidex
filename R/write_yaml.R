d445ccc2f13c6c2bb0093838e9699039bf81d459
Bas Latcham <bas@latcham.au>
2023-01-25 12:32:01 +1100

    style: :lipstick: run styler

--- R/write_yaml.R
+++ R/write_yaml.R
@@ -1,4 +1,3 @@
-
 #' Write the YAML Code for CSS Theme
 #'
 #' @param theme The name of the theme, passed as a single string. Note that
@@ -6,17 +5,24 @@
 #'  be included.
 #'
 write_theme <- function(theme) {
-  css_dir <- system.file('rmarkdown', 'templates', 'xaringan', 'resources',
-                       package = 'xaringan')
-  all_themes <- list.files(css_dir, '[.]css$')
+ css_dir <- system.file(
+		"rmarkdown",
+		"templates",
+		"xaringan",
+		"resources",
+		package = "xaringan"
+	)
+	all_themes <- list.files(css_dir, "[.]css$")
 
-  chosen_theme <- all_themes[grepl(theme, all_themes)]
-  chosen_theme <- gsub("\\.css", "", chosen_theme)
+ chosen_theme <- all_themes[grepl(theme, all_themes)]
+	chosen_theme <- gsub("\\.css", "", chosen_theme)
 
-  paste0('css: ["default", ',
-         paste0('"', chosen_theme, '"', collapse = ", "),
-         "]",
-         collapse = "")
+	paste0(
+		'css: ["default", ',
+		paste0('"', chosen_theme, '"', collapse = ", "),
+		"]",
+		collapse = ""
+	)
 }
 
 #' Create the \href{https://github.com/yihui/xaringan}{xaringan} YAML Front
@@ -41,49 +47,61 @@ write_theme <- function(theme) {
 #'   \code{"github"} flavored highlighting
 #' @keywords internal
 
-create_yaml <- function(xml_folder, title_sld, author = NULL, title = NULL,
-                        sub = NULL, date = Sys.Date(), theme = "default",
-                        highlightStyle = "github") {
-  if(is.null(author)) {
-    author <- extract_author(xml_folder)
-  }
-  if(is.null(title)) {
-    ttl  <- extract_title(title_sld)
-    ttl  <- paste0("title: '",
-                   gsub("\t|\n", "", substr(ttl, 3, nchar(ttl))),
-                   "'")
-  }
-  if(is.null(sub)) {
-    sub <- extract_subtitle(title_sld)
-  }
-  if(!is.null(title)) ttl <- paste0("title: '", title, "'")
-  if(!is.null(sub)) sub  <- paste0("subtitle: ", sub)
+create_yaml <- function(
+	xml_folder,
+	title_sld,
+	author = NULL,
+	title = NULL,
+	sub = NULL,
+	date = Sys.Date(),
+	theme = "default",
+	highlightStyle = "github"
+				) {
+	if (is.null(author)) {
+		author <- extract_author(xml_folder)
+	}
+	if (is.null(title)) {
+		ttl <- extract_title(title_sld)
+		ttl <- paste0(
+			"title: '",
+			gsub("\t|\n", "", substr(ttl, 3, nchar(ttl))),
+			"'"
+		)
+	}
+ if (is.null(sub)) {
+		sub <- extract_subtitle(title_sld)
+	}
+ if (!is.null(title)) ttl <- paste0("title: '", title, "'")
+	if (!is.null(sub)) sub <- paste0("subtitle: ", sub)
 
-  date <- paste0("date: ", date)
-  hls  <- paste0("highlightStyle: ",  highlightStyle)
+ date <- paste0("date: ", date)
+ hls <- paste0("highlightStyle: ", highlightStyle)
 
-  if(theme != "default") {
-    css  <- write_theme(theme)
-  } else {
-    css <- NULL
-  }
+ if (theme != "default") {
+		css <- write_theme(theme)
+	} else {
+		css <- NULL
+	}
 
-  if(length(author) == 1) {
-    auth <- paste0("author: ", author)
-  }
-  if(length(author) > 1) {
-    auth <- paste0("author:\n",
-                   paste0("  - ", author, collapse = "\n"))
-  }
-  elements <- list(
-			"---",
-			ttl,
-			sub,
-			auth,
-			date,
-			"format: revealjs",
-		"---")
-  elements <- elements[!map_lgl(elements, is.null)]
+ if (length(author) == 1) {
+		auth <- paste0("author: ", author)
+	}
+	if (length(author) > 1) {
+		auth <- paste0(
+			"author:\n",
+			paste0("  - ", author, collapse = "\n")
+		)
+	}
+	elements <- list(
+		"---",
+		ttl,
+		sub,
+		auth,
+		date,
+		"format: revealjs",
+		"---"
+	)
+	elements <- elements[!map_lgl(elements, is.null)]
 
-  paste0(elements, collapse = "\n")
+ paste0(elements, collapse = "\n")
 }
